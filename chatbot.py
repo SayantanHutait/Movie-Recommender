32e3bbf4a9394dda3b421a36bbeb9c53c87f1b42
SayantanHutait <sayantan344hutait@gmail.com>
2025-08-10 18:54:11 +0530

    updated UI

--- chatbot.py
+++ chatbot.py
@@ -3,10 +3,14 @@
 from langchain_groq import ChatGroq
 from langchain_tavily import TavilySearch
 from langgraph.prebuilt import create_react_agent
+from langgraph.graph import StateGraph, START, END
+from typing import TypedDict, Annotated
+from langchain_core.messages import BaseMessage, HumanMessage
+from langgraph.checkpoint.memory import InMemorySaver
+from langgraph.graph.message import add_messages
 from dotenv import load_dotenv
+
 load_dotenv()
-
-
 
 # Load LLM
 llm = ChatGroq(
@@ -17,8 +21,11 @@ llm = ChatGroq(
 search_tool = TavilySearch(
     max_results=4,
     include_image="False",
+    topic="general"
 )
 tools = [search_tool]
+
+checkpointer = InMemorySaver()
 
 # Create the React agent
 agent = create_react_agent(
@@ -28,15 +35,12 @@ agent = create_react_agent(
     keep the topic general
 You are a helpful AI assistant to give answer about movies. If you know the answer, respond directly.
 If not, use the web search tool.
-"""
+""",
+checkpointer=checkpointer
 )
-def get_response(user_input:str, chat_history:list)->tuple[str,list]:
-    chat_history.append({"role": "user", "content": user_input})
-    result = agent.invoke({"messages": chat_history})
-    assistant_msg = result["messages"][-1]
-    chat_history.append({"role": "assistant", "content": assistant_msg.content})
-    return assistant_msg.content, chat_history
 
+
+config = {"configurable": {"thread_id": "1"}}
 
 if __name__ == "__main__":
     chat_history = []  # keeps previous messages
@@ -50,13 +54,10 @@ if __name__ == "__main__":
         chat_history.append({"role": "user", "content": user_input})
 
         # Run the agent with full history
-        result = agent.invoke({"messages": chat_history})
+        result = agent.invoke({"messages": [HumanMessage(content=user_input)]}, config=config)
 
         # Extract assistant message and print it
         assistant_msg = result["messages"][-1]
         print("Bot:", assistant_msg.content)
-
-        # Add assistant response to chat history
-        chat_history.append({"role": "assistant", "content": assistant_msg.content})
 
 
--- pages/MovieBot.py
+++ pages/MovieBot.py
@@ -1,13 +1,20 @@
 # chatbot_page.py
 import streamlit as st
-from chatbot import get_response  # Your chatbot logic
+from chatbot import agent  # Your chatbot logic
+from langchain_core.messages import HumanMessage
+
 st.set_page_config(page_title="Movie ChatBot", page_icon="🎙️")
 st.title("🎙️ Movie ChatBot")
 
-st.title("🎙️ Movie ChatBot")
 
-if "chat_history" not in st.session_state:
-    st.session_state.chat_history = []
+CONFIG = {"configurable": {"thread_id": "1"}}
+
+if 'message_history' not in st.session_state:
+    st.session_state['message_history'] = []
+
+for message in st.session_state['message_history']:
+    with st.chat_message(message['role']):
+        st.text(message['content'])
 
 
 
@@ -15,16 +22,16 @@ if "chat_history" not in st.session_state:
 user_input = st.chat_input("Ask something about movies...")
 
 if user_input:
-    # Add user message
-    st.session_state.chat_history.append({"role": "user", "content": user_input})
-    with st.chat_message("user"):
-        st.markdown(user_input)
 
-    # Get bot response
-    response, updated_history = get_response(user_input, st.session_state.chat_history)
-    st.session_state.chat_history = updated_history
+    # first add the message to message_history
+    st.session_state['message_history'].append({'role': 'user', 'content': user_input})
+    with st.chat_message('user'):
+        st.text(user_input)
 
-    # Add bot message
-    st.session_state.chat_history.append({"role": "assistant", "content": response})
-    with st.chat_message("assistant"):
-        st.markdown(response)
+    response = agent.invoke({'messages': [HumanMessage(content=user_input)]}, config=CONFIG)
+    
+    ai_message = response['messages'][-1].content
+    # first add the message to message_history
+    st.session_state['message_history'].append({'role': 'assistant', 'content': ai_message})
+    with st.chat_message('assistant'):
+        st.text(ai_message)
